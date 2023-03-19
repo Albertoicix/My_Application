5054366ecfc7e8a765743198770cb91b13d194c8
Albertoicix <127573098+Albertoicix@users.noreply.github.com>
2023-03-19 21:12:59 +0900

    First Project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 이귀봉이가 git을 테스트 하고 있어요.
     }
 }
